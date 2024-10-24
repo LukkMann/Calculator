90a7943ae51cbf230bb6d685ef72133c1bb6ba09
Muhammad Lukmanhakim Bin Ahmad <lukkim46@gmail.com>
2024-10-24 11:45:32 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+            int result = 0;
+
+            string answer;
+          
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("choose operation +,-,x,/");
+            answer = Console.ReadLine();
+
+            if (answer == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (answer == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (answer == "x")
+            {
+                result = num1 * num2;
+            }
+            else if (answer == "/")
+            {
+                result = num1 / num2;
+            }
+            else 
+            {
+                Console.WriteLine("unidentified operation");
+            }
+
+            Console.WriteLine("Result " + result);
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
